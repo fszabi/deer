eb79e397bedea3a39b4019e08a99c88934c11c08
Forgacs Szabolcs <forgacsszabolcs@pm.me>
2024-01-04 21:53:58 +0100

    images

--- main.js
+++ main.js
@@ -16,14 +16,6 @@ const scrollButton = document.querySelector(".btn--scroll-top");
 
 const body = document.querySelector("body");
 
-const referenceImages = document.body.querySelectorAll(".reference img");
-
-const popup = document.querySelector(".popup-image");
-
-const popupImage = document.querySelector(".popup-image img");
-
-const closeIcon = document.querySelector(".close");
-
 scrollWatcher.setAttribute("data-scroll-watcher", "");
 
 header.before(scrollWatcher);
@@ -131,23 +123,216 @@ function events() {
     });
   });
 
-  referenceImages.forEach((image) => {
-    image.addEventListener("click", () => {
-      popup.style.visibility = "visible";
-      popup.style.opacity = "1";
-      body.style.overflow = "hidden";
-      let imageSrc = "";
+  const kislang = [
+    new URL("assets/kislang/kislang_1.webp", import.meta.url).href,
+    new URL("assets/kislang/kislang_2.webp", import.meta.url).href,
+    new URL("assets/kislang/kislang_3.webp", import.meta.url).href,
+    new URL("assets/kislang/kislang_4.webp", import.meta.url).href,
+    new URL("assets/kislang/kislang_5.webp", import.meta.url).href,
 
-      imageSrc = image.getAttribute("src");
+    // new URL("assets/kislang/kislang_1_phone.webp", import.meta.url).href,
+    // new URL("assets/kislang/kislang_2_phone.webp", import.meta.url).href,
+    // new URL("assets/kislang/kislang_3_phone.webp", import.meta.url).href,
+    // new URL("assets/kislang/kislang_4_phone.webp", import.meta.url).href,
+    // new URL("assets/kislang/kislang_5_phone.webp", import.meta.url).href,
+  ];
 
-      popupImage.src = imageSrc;
+  const jaszbereny = [
+    "assets/jaszbereny/jaszbereny_1.webp",
+    "assets/jaszbereny/jaszbereny_2.webp",
+    "assets/jaszbereny/jaszbereny_3.webp",
+    "assets/jaszbereny/jaszbereny_4.webp",
+    "assets/jaszbereny/jaszbereny_5.webp",
+    "assets/jaszbereny/jaszbereny_6.webp",
+    "assets/jaszbereny/jaszbereny_7.webp",
+  ];
+
+  const petofi = [
+    "assets/petofi/petofi_1.webp",
+    "assets/petofi/petofi_2.webp",
+    "assets/petofi/petofi_3.webp",
+    "assets/petofi/petofi_4.webp",
+  ];
+
+  const tamasi = [
+    "assets/tamasi/tamasi_1.webp",
+    "assets/tamasi/tamasi_2.webp",
+    "assets/tamasi/tamasi_3.webp",
+    "assets/tamasi/tamasi_4.webp",
+    "assets/tamasi/tamasi_5.webp",
+  ];
+
+  const trianon = ["assets/trianon/trianon.webp"];
+
+  const kislangImg = document.body.querySelector(".kislang picture img");
+  const kislangPopup = document.body.querySelector(".kislang-popup");
+  const kislangPopupImg = document.body.querySelector(".kislang-popup img");
+
+  const jaszberenyImg = document.body.querySelector(".jaszbereny picture img");
+  const jaszberenyPopup = document.body.querySelector(".jaszbereny-popup");
+  const jaszberenyPopupImg = document.body.querySelector(
+    ".jaszbereny-popup img"
+  );
+
+  const petofiImg = document.body.querySelector(".petofi picture img");
+  const petofiPopup = document.body.querySelector(".petofi-popup");
+  const petofiPopupImg = document.body.querySelector(".petofi-popup img");
+
+  const tamasiImg = document.body.querySelector(".tamasi picture img");
+  const tamasiPopup = document.body.querySelector(".tamasi-popup");
+  const tamasiPopupImg = document.body.querySelector(".tamasi-popup img");
+
+  const trianonImg = document.body.querySelector(".trianon picture img");
+  const trianonPopup = document.body.querySelector(".trianon-popup");
+  const trianonPopupImg = document.body.querySelector(".trianon-popup img");
+
+  const closeIcon = document.querySelectorAll(".close");
+
+  const leftArrow = document.querySelectorAll(".arrow-left");
+
+  const rightArrow = document.querySelectorAll(".arrow-right");
+
+  kislangImg.addEventListener("click", () => {
+    kislangPopup.style.visibility = "visible";
+    kislangPopup.style.opacity = "1";
+    body.style.overflow = "hidden";
+
+    let i = 0;
+    kislangPopupImg.src = kislang[i];
+
+    rightArrow[0].addEventListener("click", () => {
+      if (i < kislang.length - 1) {
+        i++;
+        kislangPopupImg.src = kislang[i];
+        console.log(i);
+      }
+    });
+
+    leftArrow[0].addEventListener("click", () => {
+      if (i > 0) {
+        i--;
+        kislangPopupImg.src = kislang[i];
+        console.log(i);
+      }
+    });
+
+    closeIcon[0].addEventListener("click", () => {
+      kislangPopup.style.visibility = "hidden";
+      kislangPopup.style.opacity = "0";
+      body.style.overflow = "visible";
+      kislangPopupImg.src = "";
     });
   });
 
-  closeIcon.addEventListener("click", () => {
-    popup.style.visibility = "hidden";
-    popup.style.opacity = "0";
-    body.style.overflow = "visible";
-    popupImage.src = "";
+  jaszberenyImg.addEventListener("click", () => {
+    jaszberenyPopup.style.visibility = "visible";
+    jaszberenyPopup.style.opacity = "1";
+    body.style.overflow = "hidden";
+
+    let i = 0;
+    jaszberenyPopupImg.src = jaszbereny[i];
+
+    rightArrow[1].addEventListener("click", () => {
+      if (i < jaszbereny.length - 1) {
+        i++;
+        jaszberenyPopupImg.src = jaszbereny[i];
+        console.log(i);
+      }
+    });
+
+    leftArrow[1].addEventListener("click", () => {
+      if (i > 0) {
+        i--;
+        jaszberenyPopupImg.src = jaszbereny[i];
+        console.log(i);
+      }
+    });
+
+    closeIcon[1].addEventListener("click", () => {
+      jaszberenyPopup.style.visibility = "hidden";
+      jaszberenyPopup.style.opacity = "0";
+      body.style.overflow = "visible";
+      jaszberenyPopupImg.src = "";
+    });
+  });
+
+  petofiImg.addEventListener("click", () => {
+    petofiPopup.style.visibility = "visible";
+    petofiPopup.style.opacity = "1";
+    body.style.overflow = "hidden";
+
+    let i = 0;
+    petofiPopupImg.src = petofi[i];
+
+    rightArrow[2].addEventListener("click", () => {
+      if (i < petofi.length - 1) {
+        i++;
+        petofiPopupImg.src = petofi[i];
+        console.log(i);
+      }
+    });
+
+    leftArrow[2].addEventListener("click", () => {
+      if (i > 0) {
+        i--;
+        petofiPopupImg.src = petofi[i];
+        console.log(i);
+      }
+    });
+
+    closeIcon[2].addEventListener("click", () => {
+      petofiPopup.style.visibility = "hidden";
+      petofiPopup.style.opacity = "0";
+      body.style.overflow = "visible";
+      petofiPopupImg.src = "";
+    });
+  });
+
+  tamasiImg.addEventListener("click", () => {
+    tamasiPopup.style.visibility = "visible";
+    tamasiPopup.style.opacity = "1";
+    body.style.overflow = "hidden";
+
+    let i = 0;
+    tamasiPopupImg.src = tamasi[i];
+
+    rightArrow[3].addEventListener("click", () => {
+      if (i < tamasi.length - 1) {
+        i++;
+        tamasiPopupImg.src = tamasi[i];
+        console.log(i);
+      }
+    });
+
+    leftArrow[3].addEventListener("click", () => {
+      if (i > 0) {
+        i--;
+        tamasiPopupImg.src = tamasi[i];
+        console.log(i);
+      }
+    });
+
+    closeIcon[3].addEventListener("click", () => {
+      tamasiPopup.style.visibility = "hidden";
+      tamasiPopup.style.opacity = "0";
+      body.style.overflow = "visible";
+      tamasiPopupImg.src = "";
+    });
+  });
+
+  trianonImg.addEventListener("click", () => {
+    trianonPopup.style.visibility = "visible";
+    trianonPopup.style.opacity = "1";
+    body.style.overflow = "hidden";
+
+    let i = 0;
+    trianonPopupImg.src = trianon[i];
+
+    closeIcon[4].addEventListener("click", () => {
+      trianonPopup.style.visibility = "hidden";
+      trianonPopup.style.opacity = "0";
+      body.style.overflow = "visible";
+      trianonPopupImg.src = "";
+    });
   });
 }
